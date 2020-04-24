0a96c923ff1334c23f72a907b7ae8e93335b6e33
Otávio Simões Silveira <otavios.s@hotmail.com>
2020-04-24 18:51:18 -0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    readme = fh.read()
+
 setup(name='lerMicrodados',
     version='0.1',
     url='https://github.com/otavio-s-s/lerMicrodados',
     license='MIT License',
     author='Otávio Silveira',
-    long_description=['readme.md'],
+    long_description=readme,
     long_description_content_type="text/markdown",
     author_email='otavios.s@hotmail.com',
     keywords=['Microdados', 'PNAD', 'POF'],
     description='Este pacote contém funções para leitura de microdados da POF e PNAD.',
     packages=['lerMicrodados'],
-    install_requires=['pandas', 'zipfile'])
+    install_requires=['pandas'])
